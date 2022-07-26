530af4ef295b6c39a65a205226b1f173ec073622
afisher93 <106286620+afisher93@users.noreply.github.com>
2022-07-25 20:21:39 -0500

    completed

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(num) {
+  return function (plusnumber) {
+    return num + plusnumber;
+  };
+}
+
+const plus2 = plus(2);
